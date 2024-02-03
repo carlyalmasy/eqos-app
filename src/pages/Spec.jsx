b803e18b9da9a4de1a822e5b7c52fe676369f320
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2024-02-03 09:07:36 -0800

    Some comments

--- src/pages/Spec.jsx
+++ src/pages/Spec.jsx
@@ -4,9 +4,20 @@ import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import axios from 'axios';
 
+//
+// Initialize our signal with an empty string.  On initial page load the <Markdown> component
+// below will be rendered with no content.
+//
 const data = signal('');
 
 export default function Spec() {
+
+  //
+  // Initiate a request out to our Specification document (written in markdown).  This is an
+  // asynchronous request.  Once the request completes we will re-assign the data of the
+  // request to the `data.value` property which will trigger the <Markdown> component to
+  // re-render based on the signa.
+  //
   axios
     .get(import.meta.env.VITE_CORE_URL + '/spec.md')
     .then((response) => {
