5ae2d73d8aff61cb1e2fa232da680dcc82f50632
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2024-02-12 13:59:05 -0800

    Some grid cleanup and mobile stuff

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,40 +5,39 @@ export default function Home() {
   return (
     <>
 
-      <div className="grid grid-cols-12 grid-rows-2 gap-16 grid-rows-none">
-          <div className="col-span-6">
+      <div className="grid md:grid-cols-12 grid-rows-none gap-16">
+          <div className="md:col-span-6">
             <Markdown>
               {Intro}
             </Markdown>
           </div>
-          <div className="col-span-6 col-start-7">
+          <div className="md:col-span-6">
             <div className="h-full w-full bg-slate-100">
-
             </div>
           </div>
-          <div className="col-span-8 row-start-2">
+          <div className="md:col-span-8">
             <h3>
               Title for Data Visualizations
             </h3>
 
-            <div className="grid grid-cols-12 grid-rows-2 gap-4">
-                <div className="col-span-6">
-                  <img src="https://placehold.co/600x400" />
+            <div className="grid md:grid-cols-12 grid-rows-none gap-4">
+                <div className="md:col-span-6">
+                  <img src="https://placehold.co/600x400?text=1" />
                 </div>
-                <div className="col-span-6 col-start-7">
-                  <img src="https://placehold.co/600x400" />
+                <div className="md:col-span-6">
+                  <img src="https://placehold.co/600x400?text=2" />
                 </div>
-                <div className="col-span-6 row-start-2">
-                  <img src="https://placehold.co/600x400" />
+                <div className="md:col-span-6">
+                  <img src="https://placehold.co/600x400?text=3" />
                 </div>
-                <div className="col-span-6 col-start-7 row-start-2">
-                  <img src="https://placehold.co/600x400" />
+                <div className="md:col-span-6">
+                  <img src="https://placehold.co/600x400?text=4" />
                 </div>
             </div>
 
 
           </div>
-          <div className="col-span-4 col-start-9 row-start-2">
+          <div className="md:col-span-4">
             <h3>
               Discovery Shortcuts
             </h3>
