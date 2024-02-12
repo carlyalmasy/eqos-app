8301e85719bf2392079235b964d2b021e7f6d85d
carlyalmasy <calmasy@jff.org>
2024-02-12 15:23:21 -0500

    adding resources, credentials, and detail pages with placeholders; adding new pages to router

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import PageContainer from './layouts/PageContainer';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Credentials from './pages/Credentials';
+import Detail from './pages/Detail';
+import Resources from './pages/Resources';
 import Docs from './pages/Docs';
 import Spec from './pages/Spec';
 
@@ -15,6 +18,9 @@ function App({router}) {
         <PageContainer>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/credentials" element={<Credentials />} />
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/resources" element={<Resources />} />
             <Route path="/docs" element={<Docs />} />
             <Route path="/spec" element={<Spec />} />
           </Routes>
