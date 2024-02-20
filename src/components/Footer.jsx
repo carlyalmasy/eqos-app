f351cd198b9fe4a2d2845e9276836286c5a1e751
carlyalmasy <calmasy@jff.org>
2024-02-20 15:27:27 -0500

    updated header; need to make responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,22 +2,27 @@ import PageContainer from "../layouts/PageContainer";
 
 export default function Footer() {
   const today = new Date();
-
   return (
-    <>
-      <footer className="bg-gray-200 flex">
-        <PageContainer>
-          <p className="flex-1">
-            <a href="https://eqos.org/">EQOS.ORG</a>
-          </p>
-          <p className="flex-1">
-            <a href="https://eqos.org/contact-us">CONTACT@EQOS.ORG</a>
-          </p>
-          <p className="flex-1">
-              Copyright &copy; 2023 - {today.getFullYear()} EQOS
-          </p>
-        </PageContainer>
-      </footer>
-    </>
-  )
+    <footer className="bg-gray-200 mt-16">
+      <PageContainer>
+        <div className="grid grid-cols-12 grid-rows-1 gap-4 text-sm text-gray-500 py-3">
+          <div>
+            <p>
+              <a href="https://eqos.org/">EQOS.ORG</a>
+            </p>
+          </div>
+          <div>
+            <p>
+              <a href="https://eqos.org/contact-us">CONTACT@EQOS.ORG</a>
+            </p>
+          </div>
+          <div className="col-span-10 text-right">
+            <p>&copy; 2023 - {today.getFullYear()} EQOS</p>
+          </div>
+        </div>
+      </PageContainer>
+    </footer>
+  );
 }
+
+    
