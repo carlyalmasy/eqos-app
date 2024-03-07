e52620fb0f666c02ebe5f7d91b044dcc9c3a970b
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2024-03-07 08:28:55 -0800

    Better pagination

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,35 +1,44 @@
 import { useEffect } from "react";
-import { useSignal } from "@preact/signals-react";
+import { useComputed, useSignal, useSignalEffect } from "@preact/signals-react";
 import axios from "axios";
 import CredentialsCard from "./CredentialsCard";
 import Grid from "./layout/Grid";
 import Pagination from "./Pagination";
 
+const baseUrl = import.meta.env.VITE_CORE_URL;
+
 export default function SearchResults() {
     const url = import.meta.env.VITE_CORE_URL + "/api/app/search?occupation=110";
     const items = useSignal([]);
+    const meta = useSignal({});
     const currentPage = useSignal(1);
-    const itemsPerPage = useSignal(9);
 
-    useEffect(() => {
+    useSignalEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+
+        params.set('p', currentPage.value);
+
         axios
-            .get(url)
+            .get(new URL('/api/app/search?' + params, baseUrl))
             .then((response) => {
                 items.value = response.data.data;
+                meta.value  = response.data.meta;
             })
-    }, []);
+    });
 
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = items.value.slice(indexOfFirstItem, indexOfLastItem);
-    const nPages = Math.ceil(items.value.length / itemsPerPage);
-    console.log(currentItems)
+    const nPages = useComputed(() => {
+        if (meta.value.total) {
+            return Math.ceil(meta.value.total / meta.value.limit);
+        }
+
+        return 0;
+    });
 
     return (
         <>
             <Grid split="3" className="auto-rows-fr">
-                {items.value.map((currentItems) => (
-                    <CredentialsCard key={currentItems.id} data={currentItems}/>
+                {items.value.map((currentItem) => (
+                    <CredentialsCard key={currentItem.id} data={currentItem}/>
                 ))}
             </Grid>
             <div className="mt-10">
