c99a7e4cc642f18e7a1e3589ed447e70f159c6d0
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2024-03-28 12:09:12 -0700

    Some doc color work

--- src/pages/API.jsx
+++ src/pages/API.jsx
@@ -1,11 +1,12 @@
 import { RedocStandalone } from "redoc";
+import config from "../../redocly.config.js";
 
 const docsUrl = import.meta.env.VITE_CORE_URL + "/openapi.yml";
 
 export default function API() {
     return (
-        <>
-            <RedocStandalone specUrl={docsUrl} />
-        </>
+        <div class="api">
+            <RedocStandalone specUrl={docsUrl} options={config} />
+        </div>
     );
 }
