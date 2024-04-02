46147061f6c41e041c49f4158c53df6c8bc6fb8b
carlyalmasy <calmasy@jff.org>
2024-04-02 19:50:51 -0400

    very important 404 update

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,5 @@
 import PrimaryButton from "../components/PrimaryButton"
-import SecondaryButton from "../components/SecondaryButton"
+import Fire from "../assets/images/404.gif"
 
 export default function NotFound() {
     return (
@@ -7,6 +7,7 @@ export default function NotFound() {
         <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
           <div className="text-center">
             <p className="text-lg font-semibold text-eqos-600">404</p>
+            <img src={Fire} />
             <h1 className="mt-4 text-3xl font-bold tracking-tight text-neutrals-dark-500 sm:text-5xl">Page not found</h1>
             <p className="mt-6 text-base leading-7 text-neutrals-dark-100">Sorry, we couldn't find the page you're looking for.</p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
