de4661235244edd0fa7528c4660e12d29419a69e
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2024-04-03 12:14:30 -0700

    More easter eggy

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,12 +4,14 @@ import Fire from "../assets/images/404.gif"
 export default function NotFound() {
     return (
       <>
-        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <main className="grid min-h-full place-items-center bg-white px-6 lg:px-8">
           <div className="text-center">
             <p className="text-lg font-semibold text-eqos-600">404</p>
-            <img src={Fire} />
-            <h1 className="mt-4 text-3xl font-bold tracking-tight text-neutrals-dark-500 sm:text-5xl">Page not found</h1>
-            <p className="mt-6 text-base leading-7 text-neutrals-dark-100">Sorry, we couldn't find the page you're looking for.</p>
+            <h1 className="mt-4 text-3xl font-bold tracking-tight text-neutrals-dark-500 sm:text-5xl">Page Not Found</h1>
+            <div className="group">
+                <p className="mt-6 text-base leading-7 text-neutrals-dark-100">Sorry, we couldn't find the page you're looking for.</p>
+                <img className="h-0 opacity-0 transition-all duration-1000 group-hover:h-auto group-hover:opacity-100 w-[100vw]" src={Fire} />
+            </div>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <PrimaryButton link="/" text="Go back home"/>
             </div>
