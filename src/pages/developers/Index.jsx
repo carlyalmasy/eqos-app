589ffc83f3cf919047febb1d070bdf995c418a66
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2024-04-05 11:49:01 -0700

    Initial developers language

--- src/pages/developers/Index.jsx
+++ src/pages/developers/Index.jsx
@@ -7,6 +7,8 @@ import DeveloperIntro from "../../topics/Developer/DeveloperIntro.md";
 
 import { NavLink } from "react-router-dom";
 
+const baseUrl = import.meta.env.VITE_CORE_URL;
+
 export default function Developer() {
     return (
         <Container className="lg:flex justify-start space-x-0 lg:space-x-14">
@@ -15,10 +17,58 @@ export default function Developer() {
             </div>
             <Card color="silver-100" borderRadius="md" dropShadow="none" marginTop="14" maxWidth="500px">
                 <div className="p-8">
-                    <h4 className="mt-0 text-neutrals-dark-600">API Documentation</h4>
-                    <p>A description of what you will find at the two links below--just enough information so I know what these contain.</p>
-                    <p><NavLink to="api">API Outline</NavLink></p>
-                    <p><NavLink to="spec">API Specification</NavLink></p>
+                    <h4 className="mt-0 text-neutrals-dark-600">Quick Links</h4>
+                    <p>
+                        Use the links below to get started quickly and easily.  Got questions?
+                        Contact us at <a href="mailto:support@eqos.org">support@eqos.org</a>.
+                    </p>
+                    <ul>
+                        <li>
+                            Developer References
+                            <ul>
+                                <li>
+                                    <p>
+                                        <NavLink to="/developers/api">API Documentation</NavLink>
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        <NavLink to="/developers/spec">EQOS Specification</NavLink>
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        <NavLink to="/devleopers/getting-started">Getting Started Guide</NavLink>
+                                    </p>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            Resources
+                            <ul>
+                                <li>
+                                    <p>
+                                        <a href={ baseUrl + '/openapi.yml' }>OpenAPI Specification (YML)</a>
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        <a href={ baseUrl + '/ns/v1' }>EQOS Vocabulary (JSON)</a>
+                                    </p>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            Bulk Data
+                            <ul>
+                                <li>
+                                    <p>
+                                        <NavLink to="/devleopers/download">Download</NavLink>
+                                    </p>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
             </Card>
         </Container>
--- src/topics/Developer/DeveloperIntro.md.js
+++ src/topics/Developer/DeveloperIntro.md.js
@@ -1,13 +1,35 @@
+const baseUrl = import.meta.env.VITE_CORE_URL;
+
 export default `
-## EQOS Developer Documentation
+## EQOS Developers
 
-This content would be for developers who wish to access to the EQOS API including:
+We aim to make getting started with EQOS quality signal as easy and accessible as possible by
+implementing open industry standards.
 
-Who to contact for initial access
+### OpenAPI Specification
 
-How I should contact them (email?)
+EQOS API endpoints are described in the [OpenAPI Specification](https://swagger.io/specification/),
+so integrating into tools like Postman, Swagger, etc, is a simple copy and paste away.
 
-Troubleshooting information
+### JSON-LD
 
-Anything else related to the API we want to post publicly
+EQOS API uses JSON-LD with its own well-defined vocabulary and specification which operates as
+as supplemental vocabulary to the
+[W3C Verifiable Credentials 2.0](https://www.w3.org/TR/vc-data-model-2.0/) standard by adding meta
+data regarding credentials subjects (aligned skills, job titles, etc) and credential quality
+scoring at both the overall and dimensional level.
+
+### REST
+
+EQOS API is implemented as industry standard HTTP REST endpoints with robust query parameters for
+searching from almost every perspective (credentials, skills, providers, etc).  The API itself as
+well as the documentation are publicly available with limited restrictions.
+
+### Bulk Data
+
+If APIs aren't your thing, we've got you covered.  Bulk data downloads enable you to
+get the latest and greatest quality signal information in both CSV and JSON formats.  Import them
+into your current infrastructure or analyze them in Jupyter Notebooks to gain insights into the
+current credential landscape.
 `
+
