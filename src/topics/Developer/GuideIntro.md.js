a113e17e556631f6fff6c829bb4a3d6759161860
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2024-04-10 09:16:43 -0700

    Update intro (moving purpose)

--- src/topics/Developer/GuideIntro.md.js
+++ src/topics/Developer/GuideIntro.md.js
@@ -1,5 +1,10 @@
 export default `
 # EQOS Developers Guide
 
-This guide is designed to provided practical support and examples as a supplement to the [API Documentation](/developers/api).
+The purpose of this guide is to get developers started with the basic concepts of accessing and using the API by providing practical coding examples.
+
+> **NOTE:** All code examples are written in vanilla JavaScript making use of the [axios library](https://www.npmjs.com/package/axios) for making HTTP requests.
+
+Below, we'll cover several basic ways to interact with the API as well as specific use cases identified for our early adoption scenarios.  For comprehensive information about the various API endpoints, including example response structures and available parameters, developers are encouraged to review our [API Documentation](/developers/api).
+
 `
